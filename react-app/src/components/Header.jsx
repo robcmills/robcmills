46b5e230682f80c29221b3963b86a5fbe2c096de
Robert Mills <robcmills@gmail.com>
2019-12-22 15:24:27 -0800

    Implement css animation restart on click in header svg component

--- react-app/src/components/Header.jsx
+++ react-app/src/components/Header.jsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { createUseStyles } from 'react-jss';
+import cn from 'classnames';
+
+const keyframes = {
+  from: { strokeDashoffset: '100px' },
+  to: { strokeDashoffset: '0px' },
+};
 
 const useStyles = createUseStyles({
 	header: {
@@ -11,25 +17,30 @@ const useStyles = createUseStyles({
 	svg: {
 		flex: 1,
 	},
-	'@keyframes draw_in': {
-	   from: { strokeDashoffset: '100px' },
-	   to: { strokeDashoffset: '0px' },
-	},
 	g: {
-	   animation: '$draw_in 3s',
+	   animationDuration: '3s',
 	   stroke: 'white',
 	   strokeDasharray: '100px, 100px',
      strokeDashoffset: '0px',
      strokeWidth: 1,
 	},
+	'@keyframes animation1': keyframes,
+	'@keyframes animation2': keyframes,
+	animation1: { animationName: '$animation1' },
+	animation2: { animationName: '$animation2' },
 });
 
 export default function Header() {
 	const c = useStyles();
+	const [toggle, setToggle] = useState(true);
+	const handleClick = useCallback(() => {
+		setToggle(!toggle);
+	}, [toggle]);
+	const gClassName = cn(c.g, toggle ? c.animation1 : c.animation2);
   return (
-    <div className={c.header}>
+    <div className={c.header} onClick={handleClick}>
       <svg className={c.svg} viewBox="0 0 95 30" version="1.1">
-        <g className={c.g} id='robcmills'>
+        <g className={gClassName} id='robcmills'>
           <path id='ro' d='M 5,25 l 0,-5 c 0-2.76, 2.14-5, 5 -5 c 2.76 0, 5 2.14, 5 5 c 0 2.76, 2.14 5, 5 5 c 2.76 0, 5-2.14, 5-5 c 0-2.76, -2.14-5, -5-5 c -2.76 0, -5 2.14, -5 5'/>
           <path id='b' d='M 25 5 l 0 15 c 0 2.76, 2.14 5, 5 5 c 2.76 0, 5-2.14, 5-5 c 0-2.76, -2.14-5, -5-5 c -2.76 0, -5 2.14, -5 5'/>
           <path id='cmi' d='M 40 15 m 3.53 1.464 q -1.53 -1.464, -3.53 -1.464 c -2.76 0, -5 2.14, -5 5 c 0 2.76, 2.14 5, 5 5 c 2.76 0, 5-2.14, 5-5 c 0-2.76, 2.14-5, 5 -5 c 2.76 0, 5 2.14, 5 5 l 0 5 l 0 -5 c 0-2.76, 2.14-5, 5 -5 c 2.76 0, 5 2.14, 5 5 l 0 2.5 c 0 1.38, 1.07 2.5, 2.5 2.5 c 1.38 0, 2.5-1.07, 2.5-2.5 l 0 -7.5'/>
