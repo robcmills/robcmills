9e615f2f8e8ed7642758ec637f6b9a8a4443d293
robcmills <rob@buildingconnected.com>
2020-01-01 13:31:57 -0800

    Update sync script to sync react-app/build

--- sync.sh
+++ sync.sh
@@ -1,6 +1,12 @@
 
+# aws s3 sync  \
+#   /Users/robcmills/code/robcmills.net/robcmills/dist  \
+#   s3://robcmills.net  \
+#   --exclude '*.DS_Store' # \
+  # --dryrun
+
 aws s3 sync  \
-  /Users/robcmills/code/robcmills.net/robcmills/dist  \
+  /Users/robcmills/code/robcmills.net/robcmills/react-app/build  \
   s3://robcmills.net  \
   --exclude '*.DS_Store' # \
   # --dryrun
