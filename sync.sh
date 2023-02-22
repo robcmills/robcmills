49ab5b05685eb4c550922dcc46c33b8a7b9450c7
Robert Carter Mills <robcmills@gmail.com>
2023-02-22 15:03:59 -0800

    Move legacy jade files into jade dir

--- sync.sh
+++ sync.sh
@@ -1,10 +1,3 @@
-
-# aws s3 sync  \
-#   /Users/robcmills/code/robcmills.net/robcmills/dist  \
-#   s3://robcmills.net  \
-#   --exclude '*.DS_Store' # \
-  # --dryrun
-
 aws s3 sync  \
   /Users/robcmills/code/robcmills.net/robcmills/react-app/build  \
   s3://robcmills.net  \
